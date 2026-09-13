241ddd83dff66d4a7880bff949c4098db670b0c8
BlackHoleFacts <wyatt.evans417@gmail.com>
2026-09-13 17:43:02 -0400

    Delete js directory

--- js/pwa.js
+++
@@ -1,3 +0,0 @@
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker.js').catch(()=>{}),{once:true});}
-
-const pfVis=()=>document.documentElement.classList.toggle('pf-hidden-tab',document.hidden);document.addEventListener('visibilitychange',pfVis,{passive:true});pfVis();
